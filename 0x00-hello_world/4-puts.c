a4971ba79ee544ddd3b4e8c7886dbce6baa0bfc5
Mikiyas <mknewkebede@gmail.com>
2022-09-11 11:10:23 -0700

    4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,21 +3,14 @@
 
 
 /**
- *
- *  *main - Entry point
- *
- *   *Return: Always 0 (Success)
- *
- *    */
+ * Description: main - Prints out a string to stdout.
+ * Return: 0 if Success.
+ */
 
 
 
-int main(void)
-
-{
-
-		puts("\"Programming is like building a multilingual puzzle");
-
-			return (0);
+int main (void) {
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
 
 }
