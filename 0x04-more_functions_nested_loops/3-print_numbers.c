d1ae30e74902b36741fe992bdd903afef410d6b3
Miki0035 <mknewkebede@gmail.com>
2023-02-23 08:12:40 -0800

    3

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,14 +1,14 @@
 #include "main.h"
 
 /**
- * print_numbers - prints number 0 to 9
+ * print_numbers - prints number
  */
 
 void print_numbers(void)
 {
 	int i;
 
-	for (i = 48; i <= 57; i++)
+	for (i = 48; i < 58; i++)
 	{
 		_putchar(i);
 	}
